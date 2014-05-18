cce6034877785b619b2c087f81612b0afdd38605
Jan Wozniak <wozniak.jan@gmail.com>
2014-05-18 23:37:05 +0200

    added index page basic design

---
+++ priv/www/webchat/js/index.js
@@ -0,0 +1,9 @@
+function join_room() {
+    address = $("#room_name").val();
+    window.location.href = address;
+}
+
+function join_room() {
+    address = $("#room_name").val();
+    window.location.href = address;
+}

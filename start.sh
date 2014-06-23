fafc542c6c18cbf351b02b8d39a94db2833a1752
Jan Wozniak <wozniak.jan@gmail.com>
2014-06-23 15:15:08 +0200

    node&cookie getting from env. vars

--- start.sh
+++ start.sh
@@ -1,1 +1,9 @@
-erl -pa ebin deps/*/ebin -noshell -noinput -s camchat
+app_params="-s camchat"
+if [ -n "$COOKIE" ]; then
+    app_params="$app_params -setcookie $COOKIE"
+fi
+if [ -n "$NODE" ]; then
+    app_params="$app_params -name $NODE"
+fi
+
+erl -pa ebin deps/*/ebin -noshell -noinput $app_params

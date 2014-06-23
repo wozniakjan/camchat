0a792a4dfd9918225603fac87d211ff8e63ba978
Jan Wozniak <wozniak.jan@gmail.com>
2014-06-23 16:13:07 +0200

    fixed stability issue with malformed messages

--- src/camchat_sockjs.erl
+++ src/camchat_sockjs.erl
@@ -15,19 +15,21 @@ camchat_sockjs(Conn, closed, _State) ->
             lager:error("Disconnection ~p: ~p",[TypeOfError, Exception])
     end;
 
-camchat_sockjs(Conn, {recv, Data}, init) ->
+camchat_sockjs(Conn, {recv, Data}, State) ->
     Msg = jiffy:decode(Data),
-    parse_msg(Conn, Msg);
-camchat_sockjs(Conn, {recv, Data}, waiting_for_media) ->
-    Msg = jiffy:decode(Data),
-    parse_msg(Conn, Msg);
-camchat_sockjs(Conn, {recv, Data}, connected) ->
-    Msg = jiffy:decode(Data),
-    parse_msg(Conn, Msg).
+    try_parse_msg(Conn, Msg, State).
 
-parse_msg(_Conn, {[{<<"connect">>, _Room}]}) ->
+try_parse_msg(Conn, Msg, Status) ->
+    try parse_msg(Conn, Msg, Status) of
+        {ok, Type} -> {ok, Type}
+    catch
+        TypeOfError:Exception ->
+            lager:error("Unable to process msg: ~p - ~p: ~p",[Msg, TypeOfError, Exception])
+    end.
+
+parse_msg(_Conn, {[{<<"connect">>, _Room}]}, init) ->
     {ok, waiting_for_media};
-parse_msg(Conn, {[{<<"ready">>, Room}]}) ->
+parse_msg(Conn, {[{<<"ready">>, Room}]}, waiting_for_media) ->
     {ok, RoomStatus, User} = rooms:connect(Room, Conn, []),
     UserId = User#user.user_id,
     UN = User#user.username,
@@ -35,7 +37,7 @@ parse_msg(Conn, {[{<<"ready">>, Room}]}) ->
     UsernameList = lists:map(fun(X)-> {X#user.user_id, X#user.username} end, PeerList),
     Conn:send(jiffy:encode({[{connected, RoomStatus}, {user_id, UserId}, {user_name, UN}, {peer_list, {UsernameList}}]})),
     {ok, connected};
-parse_msg(Conn, {[{<<"ready">>, Room}, {<<"user_name">>, UN}]}) ->
+parse_msg(Conn, {[{<<"ready">>, Room}, {<<"user_name">>, UN}]}, waiting_for_media) ->
     {ok, RoomStatus, User} = rooms:connect(Room, Conn, []),
     UserId = User#user.user_id,
     rooms:edit_user(Conn, ?USERNAME_POS, UN),
@@ -43,33 +45,30 @@ parse_msg(Conn, {[{<<"ready">>, Room}, {<<"user_name">>, UN}]}) ->
     UsernameList = lists:map(fun(X)-> {X#user.user_id, X#user.username} end, PeerList),
     Conn:send(jiffy:encode({[{connected, RoomStatus}, {user_id, UserId}, {user_name, UN}, {peer_list, {UsernameList}}]})),
     {ok, connected};
-parse_msg(_Conn, {[{<<"offer">>, Offer}, {<<"caller">>, Caller}, {<<"callee">>, Callee}]}) ->
+parse_msg(_Conn, {[{<<"offer">>, Offer}, {<<"caller">>, Caller}, {<<"callee">>, Callee}]}, connected) ->
     CalleeConn = rooms:get_conn_by_user_id(Callee),
     CalleeConn:send(jiffy:encode({[{<<"offer">>, Offer}, {<<"caller">>, Caller}, {<<"callee">>, Callee}]})),
     {ok, connected};
-parse_msg(_Conn, {[{<<"answer">>, Answer}, {<<"caller">>, Caller}, {<<"callee">>, Callee}]}) ->
+parse_msg(_Conn, {[{<<"answer">>, Answer}, {<<"caller">>, Caller}, {<<"callee">>, Callee}]}, connected) ->
     CallerConn = rooms:get_conn_by_user_id(Caller),
     CallerConn:send(jiffy:encode({[{<<"answer">>, Answer}, {<<"caller">>, Caller}, {<<"callee">>, Callee}]})),
     {ok, connected};
-parse_msg(_Conn, {[{<<"ice_candidate">>, IC}, {<<"caller">>, From}, {<<"callee">>, To}]}) ->
+parse_msg(_Conn, {[{<<"ice_candidate">>, IC}, {<<"caller">>, From}, {<<"callee">>, To}]}, connected) ->
     ToConn = rooms:get_conn_by_user_id(To),
     ToConn:send(jiffy:encode({[{<<"ice_candidate">>, IC}, {<<"caller">>, From}, {<<"callee">>, To}]})),
     {ok, connected};
-parse_msg(Conn, {[{<<"change_name">>, NewName}]}) ->
+parse_msg(Conn, {[{<<"change_name">>, NewName}]}, connected) ->
     rooms:edit_user(Conn, ?USERNAME_POS, NewName),
     Id = rooms:get_user_id_by_conn(Conn),
     send_peers(Conn, jiffy:encode({[{<<"change_name">>, NewName}, {<<"id">>, Id}]})),
     {ok, connected};
-parse_msg(Conn, {[{<<"audio_energy">>, Energy}]}) ->
+parse_msg(Conn, {[{<<"audio_energy">>, Energy}]}, connected) ->
     Id = rooms:get_user_id_by_conn(Conn),
     send_peers(Conn, jiffy:encode({[{<<"audio_energy">>, Energy}, {<<"id">>, Id}]})),
     {ok, connected};
-parse_msg(Conn, {[{<<"select_stream">>, Stream}]}) ->
+parse_msg(Conn, {[{<<"select_stream">>, Stream}]}, connected) ->
     Id = rooms:get_user_id_by_conn(Conn),
     send_peers(Conn, jiffy:encode({[{<<"select_stream">>, Stream}, {<<"id">>, Id}]})),
-    {ok, connected};
-parse_msg(_Conn, Msg) ->
-    lager:info("Unknown message ~n~p~n", [Msg]),
     {ok, connected}.
 
 gracefully_close(Conn) ->

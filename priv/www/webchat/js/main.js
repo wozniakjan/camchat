fa7d9b41ddf081d6aecf308277a5a47b1c57a654
Jan Wozniak <wozniak.jan@gmail.com>
2014-10-02 11:19:42 -0400

    added ice servers

--- priv/www/webchat/js/main.js
+++ priv/www/webchat/js/main.js
@@ -14,9 +14,43 @@ var drag = undefined;
 var slide_callback = undefined;
 var x, y;
 
-var pc_config = webrtcDetectedBrowser === 'firefox' ?
-    {'iceServers': [{'url': 'stun:23.21.150.121'}]} : 
-    {'iceServers': [{'url': 'stun:stun.l.google.com:19302'}]};
+var ice_server_list = [{url:'stun:stun01.sipphone.com'},
+{url:'stun:stun.ekiga.net'},
+{url:'stun:stun.fwdnet.net'},
+{url:'stun:stun.ideasip.com'},
+{url:'stun:stun.iptel.org'},
+{url:'stun:stun.rixtelecom.se'},
+{url:'stun:stun.schlund.de'},
+{url:'stun:stun.l.google.com:19302'},
+{url:'stun:stun1.l.google.com:19302'},
+{url:'stun:stun2.l.google.com:19302'},
+{url:'stun:stun3.l.google.com:19302'},
+{url:'stun:stun4.l.google.com:19302'},
+{url:'stun:stunserver.org'},
+{url:'stun:stun.softjoys.com'},
+{url:'stun:stun.voiparound.com'},
+{url:'stun:stun.voipbuster.com'},
+{url:'stun:stun.voipstunt.com'},
+{url:'stun:stun.voxgratia.org'},
+{url:'stun:stun.xten.com'},
+{url: 'stun:23.21.150.121'},
+{
+    url: 'turn:numb.viagenie.ca',
+    credential: 'muazkh',
+    username: 'webrtc@live.com'
+},
+{
+    url: 'turn:192.158.29.39:3478?transport=udp',
+    credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+    username: '28224511:1379330808'
+},
+{
+    url: 'turn:192.158.29.39:3478?transport=tcp',
+    credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+    username: '28224511:1379330808'
+}];
+
+var pc_config = {'iceServers': ice_server_list};
 
 function log(string, priority) {
     if(priority < LOG_LEVEL) {
